ca6153112b3acd88196fbad1668c934cafef26f0
Emin Martinian <emin.martinian@gmail.com>
2021-10-10 12:28:08 -0400

    Fix bug in that we need "password" not "pass"

--- main.go
+++ main.go
@@ -8,7 +8,15 @@ import (
 
 func main() {
 	for _, section := range config.FileSections() {
-		pass := config.FileGet(section, "pass")
-		fmt.Println(section, obscure.MustReveal(pass))
+		// can change "password" to "password2" if you want
+		pass := config.FileGet(section, "password")
+		fmt.PrintLn("showing section")
+		fmt.PrintLn(section)
+		if pass == "" {
+			fmt.PrintLn("skipping section because empty pass")
+		} else {
+			fmt.PrintLn("password is:")
+			fmt.Println(section, obscure.MustReveal(pass))
+		}
 	}
 }
